dad80a01fa818510e1f5e63e05d0d0b3a9b9b65f
badbadalexander <lijiahao0325@gmail.com>
2019-01-09 16:52:35 -0500

    Update extract.py
    The result will show the file and the specific number of the slide which contain the keyword

--- extract.py
+++ extract.py
@@ -4,6 +4,8 @@ import os
 # Get absolute path
 file_path = input("Path: ")
 print(file_path)
+key = input("Keyword: ")
+print(key)
 dirs = os.listdir(file_path)
 
 # C:\Users\94231\Documents\Python Scripts\Locator\temp\
@@ -12,16 +14,21 @@ dirs = os.listdir(file_path)
 text_runs = []
 for file in dirs:
     if file.endswith('.pptx'):
-        print(file)
+        #print(file)
         the_file = Presentation(file_path + file)
+        i = 0
         for slide in the_file.slides:
-            #print("this is slide " + )
+            i = i+1
             for shape in slide.shapes:
                 if not shape.has_text_frame:
                     continue
                 for paragraph in shape.text_frame.paragraphs:
                     for run in paragraph.runs:
-                        text_runs.append(run.text)
+                        #text_runs.append(run.text)
+                        if run.text.find(key) != -1:
+                            text_runs.append(file+"-slide "+str(i))
+
 
 for x in text_runs:
     print(x)
+
